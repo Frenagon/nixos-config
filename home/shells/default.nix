7752c37c569b667efaa9caea37863edb2ae2b7fd
Francisco Aceves <frenagon@gmail.com>
2024-12-23 01:11:32 -0600

    zsh: add oh-my-zsh and plugins

--- home/shells/default.nix
+++ home/shells/default.nix
@@ -1,10 +1,10 @@
 {...}: {
   imports = [
     ./starship
+    ./zsh
   ];
 
   programs.bash.enable = true;
-  programs.zsh.enable = true;
 
   # Extra directories to add to PATH.
   home.sessionPath = [
