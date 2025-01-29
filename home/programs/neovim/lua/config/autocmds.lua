fa8cb1d4293eab5762cf1181fdc678d41a092c3a
Francisco Aceves <frenagon@gmail.com>
2025-01-28 19:12:34 -0600

    neovim: load shada after ui-enter

---
+++ home/programs/neovim/lua/config/autocmds.lua
@@ -0,0 +1,9 @@
+local shada = vim.o.shada
+vim.o.shada = ""
+vim.api.nvim_create_autocmd("User", {
+	pattern = "VeryLazy",
+	callback = function()
+		vim.o.shada = shada
+		pcall(vim.cmd.rshada, { bang = true })
+	end,
+})
