549133b3a566b15351143cf29be2fad14818580e
Francisco Aceves <frenagon@gmail.com>
2025-01-28 19:10:05 -0600

    neovim: set diagnostic options

--- home/programs/neovim/lua/config/options.lua
+++ home/programs/neovim/lua/config/options.lua
@@ -11,3 +11,24 @@ vim.o.shiftwidth = 2
 vim.o.termguicolors = true
 
 vim.o.mouse = "a"
+
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "",
+		suffix = "",
+		format = function(diagnostic)
+			return " " .. diagnostic.message .. " "
+		end,
+	},
+	underline = {
+		severity = { min = vim.diagnostic.severity.WARN },
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.HINT] = "󱐮",
+			[vim.diagnostic.severity.ERROR] = "✘",
+			[vim.diagnostic.severity.INFO] = "◉",
+			[vim.diagnostic.severity.WARN] = "",
+		},
+	},
+})
