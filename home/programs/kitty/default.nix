8eafd5025199ab388d550219dc5f7805a0c76aa2
Francisco Aceves <frenagon@gmail.com>
2025-01-28 21:28:45 -0600

    kitty: add horizontal margin

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -1,6 +1,9 @@
 {...}: {
   programs.kitty = {
     enable = true;
+    extraConfig = ''
+      window_margin_width 0 10
+    '';
     keybindings = {
       "ctrl+c" = "copy_and_clear_or_interrupt";
     };
