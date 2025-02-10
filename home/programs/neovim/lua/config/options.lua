c16848195707856b574e798ae3ac8cfd0a927c88
Francisco Aceves <frenagon@gmail.com>
2025-02-10 00:10:25 -0600

    neovim: add custom options

--- home/programs/neovim/lua/config/options.lua
+++ home/programs/neovim/lua/config/options.lua
@@ -1,18 +1,33 @@
-vim.opt.laststatus = 3
-
-vim.opt.clipboard = "unnamedplus"
-
 vim.opt.termguicolors = true
-
-vim.opt.number = true
-
 vim.opt.signcolumn = "yes"
+vim.opt.laststatus = 3
+vim.opt.fillchars:append({ eob = " " })
+vim.opt.shortmess:append("aIF")
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.ruler = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.breakindent = true
+vim.opt.linebreak = true
+vim.opt.equalalways = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 2
+vim.opt.sidescrolloff = 2
 
+vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 
+vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
+vim.opt.swapfile = false
+vim.opt.undofile = true
+
+vim.opt.statusline = ""
 
 vim.diagnostic.config({
 	virtual_text = {
