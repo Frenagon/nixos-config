eddb8c5fb33e73d6a2f61dbdd5887fc9a7bae51b
Francisco Aceves <frenagon@gmail.com>
2025-02-13 18:59:29 -0600

    neovim: changed colorscheme to catppuccin

--- home/programs/neovim/lua/plugins/colorscheme.lua
+++ home/programs/neovim/lua/plugins/colorscheme.lua
@@ -1,10 +1,48 @@
 return {
 	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		enabled = true,
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+		opts = {
+			flavour = "macchiato",
+			integrations = {
+				snacks = true,
+				treesitter = true,
+				mason = true,
+				blink_cmp = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				mini = {
+					enabled = true,
+				},
+			},
+		},
+	},
+
+	{
 		"olimorris/onedarkpro.nvim",
+		enabled = false,
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme onedark")
+			vim.cmd.colorscheme("onedark")
 		end,
 	},
 }
