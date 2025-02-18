2a8802182f3ebe733de6779173195518e4b5d7ba
Francisco Aceves <frenagon@gmail.com>
2025-02-17 19:17:10 -0600

    neovim: show lsp loading state

--- home/programs/neovim/lua/config/autocmds.lua
+++ home/programs/neovim/lua/config/autocmds.lua
@@ -1,9 +1,26 @@
+local autocmd = vim.api.nvim_create_autocmd
+
 local shada = vim.o.shada
 vim.o.shada = ""
-vim.api.nvim_create_autocmd("User", {
+autocmd("User", {
 	pattern = "VeryLazy",
 	callback = function()
 		vim.o.shada = shada
 		pcall(vim.cmd.rshada, { bang = true })
+	end,
+})
+
+autocmd("LspProgress", {
+	---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+	callback = function(ev)
+		local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+		vim.notify(vim.lsp.status(), vim.log.levels.INFO, {
+			id = "lsp_progress",
+			title = "LSP Progress",
+			opts = function(notif)
+				notif.icon = ev.data.params.value.kind == "end" and " "
+					or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+			end,
+		})
 	end,
 })
