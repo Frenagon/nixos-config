a215d521678873930bcf1f7dd362711be21856bc
Francisco Aceves <frenagon@gmail.com>
2025-04-11 23:19:34 -0600

    neovim: add custom surrounding for tags

--- home/programs/neovim/lua/plugins/mini.lua
+++ home/programs/neovim/lua/plugins/mini.lua
@@ -101,7 +101,20 @@ return {
 				},
 			},
 			splitjoin = {},
-			surround = {},
+			surround = {
+				custom_surroundings = {
+					T = {
+						input = { "<(%w+)[^<>]->.-</%1>", "^<()%w+().*</()%w+()>$" },
+						output = function()
+							local tag_name = MiniSurround.user_input("Tag name")
+							if tag_name == nil then
+								return nil
+							end
+							return { left = tag_name, right = tag_name }
+						end,
+					},
+				},
+			},
 
 			-- General
 			bracketed = {},
