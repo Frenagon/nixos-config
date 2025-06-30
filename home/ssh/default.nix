5eb0e0ac4c46c98cb470361b8d18010cf69262be
Francisco Aceves <frenagon@gmail.com>
2025-06-30 01:54:52 -0600

    ssh: make github.com host a default value

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -1,8 +1,9 @@
-{...}: {
+{lib, ...}:
+with lib; {
   programs.ssh = {
     enable = true;
     matchBlocks = {
-      "github.com" = {
+      "github.com" = mkDefault {
         hostname = "github.com";
         identityFile = "~/.ssh/id_ed25519";
         identitiesOnly = true;
