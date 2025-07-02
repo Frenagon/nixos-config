d37ab0d47af260bdd414773c2667f51a947affa0
Francisco Aceves <frenagon@gmail.com>
2025-07-02 00:38:56 -0600

    host: add external monitor settings and lid switch behavior to ada-laptop

--- home/profiles/ada-laptop.nix
+++ home/profiles/ada-laptop.nix
@@ -1,10 +1,18 @@
-{...}: {
+{config, ...}: {
   imports = [./common.nix];
 
   hyprland = {
     enable = true;
     monitors = [
-      ", preferred, auto, 1.875"
+      "eDP-1, preferred, auto, 1.875"
+      "DP-3, 3440x1440@60, auto, 1.25"
+    ];
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    bindl = [
+      ", switch:on:Lid Switch, exec, hyprctl keyword monitor 'eDP-1, disable'"
+      ", switch:off:Lid Switch, exec, hyprctl keyword monitor '${builtins.elemAt config.hyprland.monitors 0}'"
     ];
   };
 }
