af561ecb56e98b5d7fc6ebcb1d3d8b570f41500e
Francisco Aceves <frenagon@gmail.com>
2026-04-03 19:10:04 -0600

    hyprland: add monitor config and disable finger reader on lid close

--- home/profiles/ada-laptop.nix
+++ home/profiles/ada-laptop.nix
@@ -1,11 +1,32 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  fprintd-toggle = pkgs.writeShellScriptBin "fprintd-toggle" ''
+    if [ "$1" = "close" ]; then
+      # Requires sudo/polkit because fprintd is a system service
+      sudo systemctl stop fprintd
+      sudo systemctl mask fprintd
+    elif [ "$1" = "open" ]; then
+      sudo systemctl unmask fprintd
+      sudo systemctl start fprintd
+    fi
+  '';
+in {
   imports = [./common.nix];
+
+  home.packages = [fprintd-toggle];
 
   hyprland = {
     enable = true;
     wallpapers = [
       {
         monitor = "eDP-1";
+        path = "${../../resources/wallpapers/cat-blue-eye.png}";
+      }
+      {
+        monitor = "DP-1";
         path = "${../../resources/wallpapers/cat-blue-eye.png}";
       }
       {
@@ -19,6 +40,7 @@
     ];
     monitors = [
       "eDP-1, preferred, auto, 1.875"
+      "DP-1, 3440x1440@60, auto, 1.25"
       "DP-3, 3440x1440@60, auto, 1.25"
     ];
   };
@@ -26,7 +48,9 @@
   wayland.windowManager.hyprland.settings = {
     bindl = [
       ", switch:on:Lid Switch, exec, hyprctl keyword monitor 'eDP-1, disable'"
+      ", switch:on:Lid Switch, exec, fprintd-toggle close"
       ", switch:off:Lid Switch, exec, hyprctl keyword monitor '${builtins.elemAt config.hyprland.monitors 0}'"
+      ", switch:off:Lid Switch, exec, fprintd-toggle open"
     ];
   };
 }
