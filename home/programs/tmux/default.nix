c0ff3a6db00845a56b8d8d9a7bb337364bcb3091
Francisco Aceves <frenagon@gmail.com>
2026-04-23 10:15:47 -0600

    tmux: refactor conf, save vim sessions and auto save

--- home/programs/tmux/default.nix
+++ home/programs/tmux/default.nix
@@ -5,16 +5,29 @@
     baseIndex = 1;
     shortcut = "C-a";
     terminal = "tmux-256color";
+    extraConfig = ''
+      set -g mode-key vi
+      set -as terminal-features ",xterm-256color:RGB"
+    '';
     plugins = with pkgs; [
       tmuxPlugins.sensible
-      tmuxPlugins.resurrect
-      tmuxPlugins.continuum
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+        '';
+      }
       {
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
-          set -g mode-key vi
           set -g @catppuccin_flavour "macchiato"
-          set -as terminal-features ",xterm-256color:RGB"
           set -g status-right-length 100
           set -g status-left-length 100
           set -g status-left ""
