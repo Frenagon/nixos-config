5f16893ec9993634ab93dc2e8724788d108931f3
Francisco Aceves <frenagon@gmail.com>
2026-05-05 16:04:42 -0600

    neovim: fix conflicting out of store config

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,11 +1,27 @@
-{lib, ...}: {
-  programs.neovim = with lib; {
-    enable = mkDefault true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    defaultEditor = true;
+{
+  config,
+  pkgs,
+  ...
+}:
+with config.lib.file; let
+  nvimConfig = "${config.home.homeDirectory}/.nixos-config/home/programs/neovim";
+in {
+  home = {
+    packages = with pkgs; [
+      neovim
+    ];
+
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+
+    shellAliases = {
+      vi = "nvim";
+      vim = "nvim";
+    };
+
+    file.".config/nvim".source = mkOutOfStoreSymlink "${nvimConfig}";
   };
+
   catppuccin.nvim.enable = false;
-  xdg.configFile."nvim".source = ../neovim;
 }
