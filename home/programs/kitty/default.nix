4a82a6b9d04bc3599f3b8ebc6ec6b6c3ede97294
Francisco Aceves <frenagon@gmail.com>
2026-06-18 00:40:19 -0600

    kitty: disable automatic config reload

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -7,6 +7,7 @@ with lib; {
   programs.kitty = mkIf (!config.windows.wsl) {
     enable = true;
     extraConfig = ''
+      auto_reload_config -1
       window_padding_width 20 10 0 10
     '';
     keybindings = {
